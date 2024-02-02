02879bcb1e993130ec7fd47861a11d5618949354
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-02 20:04:29 +0600

    task

--- CSE221 Lab01/Task4.py
+++ CSE221 Lab01/Task4.py
@@ -8,6 +8,7 @@ name=[0]*N
 location=[0]*N
 hour=[0]*N
 asci=[0]*N
+time=[0]*N
 for i in range(N):
     line= inpfile.readline()
     line=line.split(" ")
@@ -18,23 +19,58 @@ for i in range(N):
         if j==4:
             location[i]=line[j]
         if j==6:
+            time[i] = line[j]
+
             ln=line[j].split(":")
             hour[i]=int(ln[0])
+
 for i in range(len(name)):
     cal=0
     for j in name[i]:
         cal+=ord(j)
     asci[i]=cal
 
-
+for i in range(len(name)):
+    print(f"{name[i]}: {asci[i]}")
 for i in range(len(asci)):
     sml=i
-    for j in range(len(asci)):
-        if asci[j+1]<asci[sml]:
+    for j in range(i, len(asci)):
+        if asci[j]<asci[sml]:
             sml=j
-        if asci[j]==asci[j+1]:
-            if hour[j]<hour[j+1]:
-                tm=
+        if asci[i]==asci[j]:
+            if hour[i]<hour[j]:
+                tm=hour[i]
+                hour[i]=hour[j]
+                hour[j]=tm
 
+                tp=location[i]
+                location[i]=location[j]
+                location[j]=tp
+    tmm=asci[i]
+    asci[i]=asci[sml]
+    asci[sml]=tmm
+
+    temp=name[i]
+    name[i]=name[sml]
+    name[sml]=temp
+
+    tmp=location[i]
+    location[i]=location[sml]
+    location[sml]=tmp
+
+    tem=hour[i]
+    hour[i]=hour[sml]
+    hour[sml]=tem
+
+    tpm=time[i]
+    time[i]=time[sml]
+    time[sml]=tpm
+
+for i in range(len(asci)):
+    outfile.write(f"{name[i]} will departure for {location[i]} at {time[i]}\n")
+
+
+inpfile.close()
+outfile.close()
 
 
