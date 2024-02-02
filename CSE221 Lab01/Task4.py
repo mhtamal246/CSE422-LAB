920f0eea17dd25c587420bb1532a7fb63c7dc61f
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-02 21:03:00 +0600

    task

--- CSE221 Lab01/Task4.py
+++ CSE221 Lab01/Task4.py
@@ -1,76 +1,31 @@
 outfile=open('output4.txt','w')
 inpfile=open('input4.txt','r')
-
-
 N=int(inpfile.readline())
 
-name=[0]*N
-location=[0]*N
-hour=[0]*N
-asci=[0]*N
-time=[0]*N
+lst=[]
+
 for i in range(N):
-    line= inpfile.readline()
+    line=inpfile.readline()
     line=line.split(" ")
+    lst.append(line)
 
-    for j in range(len(line)):
-        if j==0:
-            name[i]=line[j]
-        if j==4:
-            location[i]=line[j]
-        if j==6:
-            time[i] = line[j]
-
-            ln=line[j].split(":")
-            hour[i]=int(ln[0])
-
-for i in range(len(name)):
-    cal=0
-    for j in name[i]:
-        cal+=ord(j)
-    asci[i]=cal
-
-for i in range(len(name)):
-    print(f"{name[i]}: {asci[i]}")
-for i in range(len(asci)):
+for i in range(len(lst)):
     sml=i
-    for j in range(i, len(asci)):
-        if asci[j]<asci[sml]:
+    for j in range(i,len(lst)):
+        if lst[j][0]<lst[sml][0]:
             sml=j
-        if asci[i]==asci[j]:
-            if hour[i]<hour[j]:
-                tm=hour[i]
-                hour[i]=hour[j]
-                hour[j]=tm
 
-                tp=location[i]
-                location[i]=location[j]
-                location[j]=tp
-    tmm=asci[i]
-    asci[i]=asci[sml]
-    asci[sml]=tmm
+        if lst[i][0]==lst[j][0]:
+            spt=lst[i][6].split(":")
+            if lst[i][6][int(0)]>lst[j][6][int(0)]:
 
-    temp=name[i]
-    name[i]=name[sml]
-    name[sml]=temp
+                tmp=lst[j]
+                lst[j]=lst[i]
+                lst[i]=tmp
+    temp=lst[i]
+    lst[i]=lst[sml]
+    lst[sml]=temp
 
-    tmp=location[i]
-    location[i]=location[sml]
-    location[sml]=tmp
-
-    tem=hour[i]
-    hour[i]=hour[sml]
-    hour[sml]=tem
-
-    tpm=time[i]
-    time[i]=time[sml]
-    time[sml]=tpm
-
-for i in range(len(asci)):
-    outfile.write(f"{name[i]} will departure for {location[i]} at {time[i]}\n")
-
-
-inpfile.close()
-outfile.close()
-
+for i in range(len(lst)):
+    outfile.write(f"{lst[i][0]} will departure for {lst[i][4]} at {lst[i][6]}")
 
