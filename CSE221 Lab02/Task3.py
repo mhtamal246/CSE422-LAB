81ae4efaa78ed7bac5286cf58538b8760f88b5f6
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-09 02:53:32 +0600

    Update

--- CSE221 Lab02/Task3.py
+++ CSE221 Lab02/Task3.py
@@ -9,22 +9,43 @@ for i in range(N):
     S=inpfile.readline()
     a,b=S.split(" ")
     lst.append((int(a),int(b)))
-print(lst)
+
 for i in range(len(lst)):
     sml=i
-    for j in range(len(lst)):
+    for j in range(i+1,len(lst)):
         if lst[i][1]>lst[j][1]:
             sml=j
 
     temp=lst[i]
     lst[i]=lst[sml]
     lst[sml]=temp
-print(lst)
 
-idx=0
-count=0
+count=1
 mainlst=[]
-for i in range(idx,len(lst)):
-    for j in range(i+1,len(lst)):
+mainlst.append(lst[0])
 
-            pass
+c=len(lst)
+i=0
+while c>=0:
+    for j in range(len(lst)):
+        if lst[i][1]<=lst[j][0]:
+            mainlst.append(lst[j])
+            i=lst[j]
+            count+=1
+            i=j
+            break
+    c-=1
+
+#for i in range(len(lst)):
+#    for j in range(i+1,len(lst)):
+#       if lst[i]!=0 and lst[j]!=0:
+#            if lst[i][1]<=lst[j][0]:
+#                mainlst.append(lst[j])
+#                count+=1
+#                break
+#            else:
+#               lst[j]=0
+
+outfile.write(f"{count}\n")
+for i in range(len(mainlst)):
+    outfile.write(f"{mainlst[i][0]} {mainlst[i][1]}\n")
---
+++ CSE221 Lab02/Task4.py
@@ -0,0 +1,55 @@
+inpfile=open("input4.txt","r")
+outfile=open("output4.txt","w")
+inp=inpfile.readline()
+
+N,M=inp.split()
+
+N=int(N)
+M=int(M)
+lst=[]
+
+for i in range(N):
+    S=inpfile.readline()
+    a,b=S.split(" ")
+    lst.append((int(a),int(b)))
+
+for i in range(len(lst)):
+    sml=i
+    for j in range(i+1,len(lst)):
+        if lst[i][1]>lst[j][1]:
+            sml=j
+    temp=lst[i]
+    lst[i]=lst[sml]
+    lst[sml]=temp
+print(lst)###########
+
+
+menlst=[0]*M
+count=1*len(menlst)
+for i in range(len(menlst)):
+    menlst[i]=lst[i]
+
+print(menlst)###################
+
+cnt=len(lst)
+point=0
+l=len(menlst)
+while cnt>=0:
+    for i in range(l+1,len(lst)):
+        if point==len(menlst)-1:
+            point=0
+
+        if menlst[point][1]<lst[i][0]:
+            menlst[0]=lst[i]
+            point+=1
+            count+=1
+            l+=1
+
+        if menlst[point][1]>lst[i][0]:
+            menlst[1]=lst[i]
+            point+=1
+            count+=1
+            l+=1
+    cnt-=1
+
+print(count)##################
