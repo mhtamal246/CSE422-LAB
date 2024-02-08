52b1a5e91185c2b15f60fcfa4bd9f14ff0f032f5
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-08 21:02:30 +0600

    Update

--- CSE221 Lab02/Task3.py
+++ CSE221 Lab02/Task3.py
@@ -9,12 +9,22 @@ for i in range(N):
     S=inpfile.readline()
     a,b=S.split(" ")
     lst.append((int(a),int(b)))
+print(lst)
+for i in range(len(lst)):
+    sml=i
+    for j in range(len(lst)):
+        if lst[i][1]>lst[j][1]:
+            sml=j
 
-for i in
+    temp=lst[i]
+    lst[i]=lst[sml]
+    lst[sml]=temp
+print(lst)
 
 idx=0
 count=0
 mainlst=[]
 for i in range(idx,len(lst)):
     for j in range(i+1,len(lst)):
-        if lst[i]
+
+            pass
