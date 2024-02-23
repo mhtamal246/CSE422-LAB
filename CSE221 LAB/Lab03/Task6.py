c34216b1abeb69d528f921d7f4eee8c08588e7e9
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-23 22:14:39 +0600

    Update

--- CSE221 LAB/Lab03/Task6.py
+++ CSE221 LAB/Lab03/Task6.py
@@ -10,12 +10,17 @@ lst = []
 for i in num:
     lst.append(int(i))
 
-def quicksort(lst, p1, p2):
-    if p1<p2:
-        part=partition(lst, p1, p2)
-        quicksort(lst, p1, part-1)
-        quicksort(lst, part+1, p2)
-    return lst
+def quicksort(lst, p1, p2,k):
+    if p1 == p2:
+        return lst[p1]
+    part = partition(lst, p1, p2)
+
+    if k == part:
+        return lst[k]
+    elif k<part:
+        return quicksort(lst, p1, part - 1, k)
+    else:
+        return quicksort(lst, part + 1, p2, k)
 def partition(lst, p1,p2):
     pivot=lst[p1]
     i=p1+1
@@ -26,19 +31,17 @@ def partition(lst, p1,p2):
         while i<=j and lst[j]>=pivot:
             j-=1
         if i<=j:
-            temp=lst[i]
-            lst[i]=lst[j]
-            lst[j]=temp
+            lst[i], lst[j] = lst[j], lst[i]
         else:
             break
-    tmp=lst[p1]
-    lst[p1]=lst[j]
-    lst[j]=tmp
+    lst[p1], lst[j] = lst[j], lst[p1]
     return j
 
-srt=quicksort(lst, 0, len(lst)-1)
-stg=""
-for i in srt:
-    stg+= str(i)+" "
+que=inpfile.readline()
+que=int(que)
 
-outfile.write(f"{stg[:len(stg)-1]}")
+for i in range(que):
+    k=inpfile.readline()
+    k=int(k)
+    srt=quicksort(lst, 0, len(lst)-1,k-1)
+    outfile.write(f"{srt}\n")
