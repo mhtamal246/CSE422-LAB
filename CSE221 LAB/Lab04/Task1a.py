04b6105d5570d9ccd2392b557e86b91df2e6629f
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2024-02-26 03:09:31 +0600

    Update

---
+++ CSE221 LAB/Lab04/Task1a.py
@@ -0,0 +1,3 @@
+inpfile=open("input1.txt","r")
+outfile=open("output1.txt","w")
+
---
+++ CSE221 LAB/Lab04/Task1b.py
@@ -0,0 +1,34 @@
+import numpy as np
+inpfile=open("input1b.txt","r")
+outfile=open("output1b.txt","w")
+
+N=inpfile.readline()
+N=N.split(" ")
+
+node=int(N[0])
+edge=int(N[1])
+
+lstt=[0]*(node+1)
+for i in range(edge):
+    line=inpfile.readline()
+    line=line.split(" ")
+    lst=[]
+    tup=(int(line[1]),int(line[2]))
+    lst.append(tup)
+
+    if lstt[int(line[0])]==0:
+        lstt[int(line[0])]=lst
+    else:
+        lstt[int(line[0])].append(tup)
+count=0
+for i in range(len(lstt)):
+    if lstt[i]==0:
+        outfile.write(f"{count}:\n")
+        count+=1
+    else:
+        stg=""
+        for j in str(lstt[i]):
+            stg+=j
+        outfile.write(f"{count}: {stg[1:-1]}\n")
+        count+=1
+
